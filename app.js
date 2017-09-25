574954a44f535015c4530033df6f1c1508d017a4
Simon Moos <simon.moos95@gmail.com>
2017-09-26 00:21:56 +0200

    Implement complete authorization example

--- app.js
+++ app.js
@@ -1,29 +1,98 @@
 var fs = require('fs');
 var request = require('request');
 var querystring = require('querystring');
+var cookieParser = require('cookie-parser');
 
 var express = require('express');
 var app = express();
+
+///////////////////////////////////////////////////////////////////////////////
 
 var client_id = '942817181f7f4d0a92bdff56b87e1b50';
 var client_secret = fs.readFileSync('SECRET_KEY', 'utf8');
 var redirect_uri = 'http://localhost:8888/oauth-callback/';
 
-// Setup static hosting path
+var spotify_access_token_key  = 'spotify_access_token';
+var spotify_refresh_token_key = 'spotify_refresh_token';
+var spotify_api_state_key     = 'spotify_api_state';
+
+///////////////////////////////////////////////////////////////////////////////
+
+function generateRandomStateString() {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (var i = 0; i < 20; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
+function getBase64AuthorizationHeader() {
+	return 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'));
+}
+
+function setAccessTokenCookie(res, access_token, expires_in) {
+	// Make access token cookie expire, so that the refresh token must be used
+	// (remove 10 seconds so that we won't accidentally use it if it just expired)
+	var expired_by_ms = Date.now() + (expires_in * 1000.0) - 10000.0;
+	var options = { expires: new Date(expired_by_ms) };
+	res.cookie(spotify_access_token_key, access_token, options);
+}
+
+function updateAccessTokenFromRefreshToken(res, refresh_token) {
+
+	var authOptions = {
+		url: 'https://accounts.spotify.com/api/token',
+		headers: { 'Authorization': getBase64AuthorizationHeader() },
+		form: {
+			grant_type: 'refresh_token',
+			refresh_token: refresh_token
+		},
+		json: true
+	};
+
+	request.post(authOptions, function(error, response, body) {
+		if (!error && response.statusCode === 200) {
+			setAccessTokenCookie(res, body.access_token, body.expires_in);
+		} else {
+			// TODO: Handle any possible errors!
+			console.log('Error: could not get access token from refresh token!');
+		}
+	});
+
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+// Setup middleware
+app.use(cookieParser());
 app.use(express.static(__dirname + '/public'));
 
-/*
+// TODO: Change public from static hosting to be able to use this!
 app.get('/', function(req, res) {
 
-	// TODO: Do stuff!
+	var refresh_token = req.cookies ? req.cookies[spotify_refresh_token_key] : null;
+	var access_token = req.cookies ? req.cookies[spotify_access_token_key] : null;
+
+	if (!refresh_token) {
+		// NOTE: Render pre-login stuff!
+		console.log('Offline!');
+	} else {
+		if (access_token) {
+			// NOTE: Render post-login stuff!
+			console.log('Online!');
+		} else {
+			updateAccessTokenFromRefreshToken(res, refresh_token);
+			console.log('Updating an access token!');
+		}
+	}
 
 });
-*/
 
 app.get('/login', function(req, res) {
 
-	// TODO: Use state!
-	var state = '';
+	var state = generateRandomStateString();
+	res.cookie(spotify_api_state_key, state);
 
 	// TODO: Specify relevant scopes!
 	var scope = 'user-read-private user-read-email';
@@ -34,21 +103,32 @@ app.get('/login', function(req, res) {
 			client_id: client_id,
 			scope: scope,
 			redirect_uri: redirect_uri,
-			state: state
+			state: state,
+			show_dialog: true /* mostly for debugging */
 		}));
 });
 
 app.get('/oauth-callback', function(req, res) {
 
-	var error = req.query.error || null;
-	var code = req.query.code || null;
+	// Make sure state is equal
 	var state = req.query.state || null;
-
-	if (error) {
-		// TODO: Handle error, maybe redirect to some error page?
-		console.log('User did not authorize this app!');
+	var cookie_state = req.cookies ? req.cookies[spotify_api_state_key] : null;
+	if (state == null || cookie_state == null || state !== cookie_state) {
+		console.log('Error: invalid state parameter!');
+		res.status(401).send('Error: you do not have permission to access this page!');
+		return;
+	} else {
+		res.clearCookie(spotify_api_state_key);
 	}
 
+	// Check for error response
+	var error = req.query.error || null;
+	if (error) {
+		// User did not give authorization, abort
+		res.redirect('/');
+	}
+
+	var code = req.query.code || null;
 	var authOptions = {
 		url: 'https://accounts.spotify.com/api/token',
 		form: {
@@ -57,7 +137,7 @@ app.get('/oauth-callback', function(req, res) {
 			grant_type: 'authorization_code'
 		},
 		headers: {
-			'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+			'Authorization': getBase64AuthorizationHeader()
 		},
 		json: true
 	};
@@ -66,21 +146,22 @@ app.get('/oauth-callback', function(req, res) {
 		if (!err && response.statusCode === 200) {
 
 			var access_token = body.access_token;
+			var expires_in = body.expires_in;
 			var refresh_token = body.refresh_token;
 
-			// Redirect back to main application with
-			res.redirect('/?' +
-				querystring.stringify({
-					"access_token": access_token,
-					"refresh_token": refresh_token
-				}));
+			// Set cookies and redirect back to main application
+			setAccessTokenCookie(res, access_token, expires_in);
+			res.cookie(spotify_refresh_token_key, refresh_token);
+			res.redirect('/');
 
 		} else {
 			// TODO: Handle invalid_token error!
-			console.log('invalid_token error!');
+			console.log('Error: invalid_token!');
 		}
 	});
 
 });
+
+///////////////////////////////////////////////////////////////////////////////
 
 app.listen(8888);
--- public/index.js
+++ public/index.js
@@ -1,36 +1,11 @@
 (function() {
+	'use strict';
 
-	// From: https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
-	function getParameterByName(name, url) {
-		if (!url) url = window.location.href;
-		name = name.replace(/[\[\]]/g, "\\$&");
-		var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
-		var results = regex.exec(url);
-		if (!results) return null;
-		if (!results[2]) return '';
-		return decodeURIComponent(results[2].replace(/\+/g, " "));
+	function getCookie(name) {
+		var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+		if (match) return match[1];
+		else return null;
 	}
-
-	function getQueryOrStorageValueAndSave(key) {
-		var value = getParameterByName(key);
-		if (value) {
-			window.localStorage.setItem(key, value);
-			return value;
-		} else {
-			return window.localStorage.getItem(key);
-		}
-	}
-
-	function removeQueryParametersFromUrl() {
-		var idx = window.location.href.indexOf('?');
-		if (idx > 0) {
-			window.location.href = window.location.href.substring(0, idx);
-		}
-	}
-
-	var accessToken = getQueryOrStorageValueAndSave('access_token');
-	var refreshToken = getQueryOrStorageValueAndSave('refresh_token');
-	//removeQueryParametersFromUrl();
 
 	function removeElementWithId(id) {
 		var toRemove = document.getElementById(id);
@@ -38,7 +13,8 @@
 	}
 
 	// HACK: Just for simplicity, so I don't have to do any fancy rendering of stuff
-	if (accessToken && refreshToken) {
+	var cookieVal = getCookie('spotify_access_token')
+	if (cookieVal !== null) {
 		removeElementWithId('pre-login');
 	} else {
 		removeElementWithId('post-login');
